b8118d41b42add76a247e70723a6d3b5ed9f61ca
Daniel Cunha <danielsoro@gmail.com>
2018-03-03 22:36:29 -0300

    Added JavaScript Standard Style

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-const irc = require('irc');
-const commands = require('./lib/commands');
+const irc = require('irc')
+const commands = require('./lib/commands')
 
 const client = new irc.Client('irc.freenode.net', 'sorinho', {
-	channels: ['#linuxers-test'],
-	userName: 'sorinho',
-	realName: "Soro's son",
-	autoConnect: true
-});
+  channels: ['#linuxers-test'],
+  userName: 'sorinho',
+  realName: "Soro's son",
+  autoConnect: true
+})
 
 client.addListener('message', function (nick, to, text, message) {
-	let result = commands.findMessage(text, nick, to)
-	if (result) {
-		client.say(result.channel, result.msg);
-	}
-});
+  let result = commands.findMessage(text, nick, to)
+  if (result) {
+    client.say(result.channel, result.msg)
+  }
+})
 
 client.addListener('error', function (from, to, text, message) {
-	client.action(from, "Me deu dor de barriga, volto depois.");
-});
+  client.action(from, 'Me deu dor de barriga, volto depois.')
+})
--- test/commands_test.js
+++ test/commands_test.js
@@ -1,24 +1,24 @@
-const assert = require('assert');
-const commands = require('../lib/commands');
+const assert = require('assert')
+const commands = require('../lib/commands')
 
 describe('!aloha', () => {
-    it('should reply @nick Aloha!!! _m/', () => {
-        let result  = commands.findMessage("!aloha", "soro", "#linuxers");
-        let expected = {
-            channel: '#linuxers',
-            msg: 'soro Aloha!!! _m/'
-        }
-        assert.equal(JSON.stringify(expected), JSON.stringify(result));
-    });
-});
+  it('should reply @nick Aloha!!! _m/', () => {
+    let result = commands.findMessage('!aloha', 'soro', '#linuxers')
+    let expected = {
+      channel: '#linuxers',
+      msg: 'soro Aloha!!! _m/'
+    }
+    assert.equal(JSON.stringify(expected), JSON.stringify(result))
+  })
+})
 
 describe('!opa', () => {
-    it('should reply @nick Opa! :)', () => {
-        let result  = commands.findMessage("!opa", "soro", "#linuxers");
-        let expected = {
-            channel: '#linuxers',
-            msg: 'soro Opa! :)'
-        }
-       assert.equal(JSON.stringify(expected), JSON.stringify(result));
-    });
-});
+  it('should reply @nick Opa! :)', () => {
+    let result = commands.findMessage('!opa', 'soro', '#linuxers')
+    let expected = {
+      channel: '#linuxers',
+      msg: 'soro Opa! :)'
+    }
+    assert.equal(JSON.stringify(expected), JSON.stringify(result))
+  })
+})
